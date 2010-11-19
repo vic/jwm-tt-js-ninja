571798c5aba2f7ce2af59dc02d50f0e9c8dff052
Victor Hugo Borja <vborja@apache.org>
2010-11-18 23:25:19 -0600

    Added ninja image and jwm logo

---
+++ scripts.js
@@ -0,0 +1,10 @@
+new(function(){
+
+  var initialize = function(){
+    $("#footer").append("<img id=ninja-logo src='/image/ninja2.sm.jpg'>")
+    $("#footer").append("<img id=jwm-logo src='/image/jwm-logo.gif'>")
+  };
+
+  $(document).ready(initialize);
+
+})(jQuery);
