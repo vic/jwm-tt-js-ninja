cd3dc0eeeac890dd95e858800ece821cf690188c
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-22 19:41:03 -0600

    Added missing images

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+new(function($){
+
+  var initialize = function(){
+    $("#footer").append("<img id=ninja-logo src='img/ninja2.sm.jpg'>")
+    $("#footer").append("<img id=jwm-logo src='img/jwm-logo.gif'>")
+  };
+
+  $(document).ready(initialize);
+
+})(jQuery);
